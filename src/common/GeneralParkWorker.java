1d0dc10eba251bafadfca41c691c0af44153f80f
Itay Mohabati <itaybestone@gmail.com>
2024-03-02 21:22:22 +0200

    Added general park worker class

---
+++ src/common/GeneralParkWorker.java
@@ -0,0 +1,67 @@
+package common;
+
+public class GeneralParkWorker {
+	private Integer workerNumber;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String role;
+	private String userName;
+	private String password;
+	
+	public GeneralParkWorker(Integer workerNumber, String firstName, String lastName, String email, String role,
+			String userName, String password) {
+		super();
+		this.workerNumber = workerNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.role = role;
+		this.userName = userName;
+		this.password = password;
+	}
+	
+	public Integer getWorkerNumber() {
+		return workerNumber;
+	}
+	public void setWorkerNumber(Integer workerNumber) {
+		this.workerNumber = workerNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
