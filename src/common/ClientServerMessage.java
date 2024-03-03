83dd95c9dd09223ad9f925403b978d29406d482e
Itay Mohabati <itaybestone@gmail.com>
2024-03-03 19:52:27 +0200

    Added generic class for handling data and command, also a class with constant future Commands

---
+++ src/common/ClientServerMessage.java
@@ -0,0 +1,60 @@
+package common;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ClientServerMessage<T> implements Serializable{
+	//A generic class that is meant to handle data and command from server and client in a generic way
+	private Object dataTransfered;
+	private String command;
+	
+	public ClientServerMessage(Object dataTransfered, String command) {
+		this.dataTransfered = dataTransfered;
+		this.command = command;
+	}
+	
+	public ArrayList<T> convertDataToArrayList() throws Exception{
+		//Converting a generic data of some kind of objects to arrayList of them
+		return (ArrayList<T>)this.dataTransfered;
+	}
+	
+	@Override
+	public String toString() {
+		//Returning a string representation of the command and data stored in dataTranfered object
+		if((String)dataTransfered != null) {
+			return "Operation: " + command.toString() + " Input ArrayList : " + dataTransfered.toString();
+		}
+		
+		return "Operation: " + command.toString() + " Input ArrayList : " + "NULL";
+	}
+	
+	public static<T> ArrayList createDataArrayList(T... dataObject){
+		//Generic method to create an arrayList from any kind of dataobjects provided
+		
+		ArrayList<T> data = new ArrayList<>();
+		for(T param : dataObject) {
+			data.add(param);
+		}
+		
+		return data;
+		
+	}
+
+	public Object getDataTransfered() {
+		return dataTransfered;
+	}
+
+	public void setDataTransfered(Object dataTransfered) {
+		this.dataTransfered = dataTransfered;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	
+	
+}
