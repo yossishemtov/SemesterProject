17920d2c951e81ba0dde9bab4ae4eeb5b1023e3a
ShaiYos <shai44553@gmail.com>
2024-03-14 12:31:17 +0200

    submit function and style
    changed submit function and style of frame

--- src/gui/RegisterNewGuideController.java
+++ src/gui/RegisterNewGuideController.java
@@ -31,50 +31,64 @@ public class RegisterNewGuideController {
 	private JFXTextField PhoneNumber;
 	
 	 
-	 public void SubmitRegister(ActionEvent click) throws Exception {
-		 String TravelerID = ID.getText();
-		 String TravelerFirstName = FirstName.getText();
-		 String TravelerLastName = LastName.getText();
-		 String TravelerEmail = Email.getText();
-		 String TravelerPhoneNumber = PhoneNumber.getText();
+	
+	
+	public void SubmitRegister(ActionEvent click) throws Exception {
+	    String TravelerID = ID.getText();
+	    String TravelerFirstName = FirstName.getText();
+	    String TravelerLastName = LastName.getText();
+	    String TravelerEmail = Email.getText();
+	    String TravelerPhoneNumber = PhoneNumber.getText();
+
+	    // Check if any of the fields are empty
+	    if (TravelerID.isEmpty() || TravelerFirstName.isEmpty() || TravelerLastName.isEmpty() ||
+	            TravelerEmail.isEmpty() || TravelerPhoneNumber.isEmpty()) {
+	        // Show an alert indicating that all fields are required
+	        Alerts alert = new Alerts(Alerts.AlertType.ERROR, "All fields are required", "", "Please fill in all the required fields");
+	        alert.showAndWait();
+	        return; // Exit the method early, as validation failed
+	    }
+
+	    // Perform additional validation for ID and Email
+	    Alerts alertID = InputValidation.ValidateVisitorID(TravelerID);
+	    Alerts alertEmail = InputValidation.validateEmail(TravelerEmail);
+	    Boolean validID = alertID.getAlertType().toString().equals("INFORMATION");
+		Boolean validEmail = alertID.getAlertType().toString().equals("INFORMATION");
 		 
-		 Alerts alertID = InputValidation.ValidateVisitorID(TravelerID); // get the right alert
-		 Boolean validID = alertID.getAlertType().toString().equals("INFORMATION");
-		 Alerts alertEmail = InputValidation.validateEmail(TravelerEmail);
-		 Boolean validEmail = alertID.getAlertType().toString().equals("INFORMATION");
-		 try {
-			 if (validID && validEmail) {
-				 
+	    // Check if ID and Email validations passed
+	    if (!validID) {
+	        alertID.showAndWait();
+	        return; // Exit the method early, as validation failed
+	    }
 
-				 Traveler GroupGuideAttempt = new Traveler(Integer.parseInt(TravelerID), null, null, null, null, 1, 0);
+	    if (!validEmail) {
+	        alertEmail.showAndWait();
+	        return; // Exit the method early, as validation failed
+	    }
+	    
+	    // If all validations pass, proceed with registration logic
+	    try {
+	    	Traveler GroupGuideAttempt = new Traveler(Integer.parseInt(TravelerID), null, null, null, null, 1, 0);
 
-				 // send to server in order to check if traveler exists in the system
-				 ClientServerMessage<?> RegistrationAttempt = new ClientServerMessage<>(GroupGuideAttempt, Operation.GET_TRAVLER_INFO);
-			     ClientUI.clientControllerInstance.sendMessageToServer(RegistrationAttempt);
-			     // get the data return from server 
-			     Traveler TravelerFromServer = (Traveler) ClientController.data.getDataTransfered();
-			     // if traveler not exist in the system, register as a new group guide
-			     if (TravelerFromServer == null) {
-			    	 ClientServerMessage<?> RegistrateGroupGuide = new ClientServerMessage<>(GroupGuideAttempt, Operation.POST_NEW_TRAVLER_GUIDER);
-			     } else {
-			    	 // traveler exists therefore he need to go to "register exist guide"
-//			    	 ClientServerMessage<?> RegistrateGroupGuide = new ClientServerMessage<>(GroupGuideAttempt, Operation.PATCH_TRAVELER_MAKE_GROUPGUIDE);
-			     }
-				 
-			 }
-			 else {
-				 if(!validID) {
-					 alertID.showAndWait();
-				 }
-				 if(!validEmail) {
-					 alertEmail.showAndWait();
-				 }
-			 }
-		 } catch(Exception e) {
-			 System.out.print("Something went wrong while clicking on submit button, trying to register new group guide, check stack trace");
-	            e.printStackTrace();
-		 }
-	 }
+			// send to server in order to check if traveler exists in the system
+			ClientServerMessage<?> RegistrationAttempt = new ClientServerMessage<>(GroupGuideAttempt, Operation.GET_TRAVLER_INFO);
+		    ClientUI.clientControllerInstance.sendMessageToServer(RegistrationAttempt);
+		    // get the data return from server 
+		    Traveler TravelerFromServer = (Traveler) ClientController.data.getDataTransfered();
+		    // if traveler not exist in the system, register as a new group guide
+		    if (TravelerFromServer == null) {
+		    	ClientServerMessage<?> RegistrateGroupGuide = new ClientServerMessage<>(GroupGuideAttempt, Operation.POST_NEW_TRAVLER_GUIDER);
+		     } 
+		    else {
+		    	 // traveler exists therefore we changed him to be group guide
+		    	 ClientServerMessage<?> RegistrateGroupGuide = new ClientServerMessage<>(GroupGuideAttempt, Operation.POST_EXISTS_TRAVLER_GUIDER);
+		     }
+	    } catch (Exception e) {
+	        System.out.print("Something went wrong while clicking on submit button, trying to register new group guide, check stack trace");
+	        e.printStackTrace();
+	    }
+	}
+
 	 
 	 public void BackBtn(ActionEvent click) throws Exception{
 			//Function for opening a new scene when clicking on the Back Button
