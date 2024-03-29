943d255e8cd5f8a8f5e1ab8010a38db0f715f6a8
yossiShemTov <yossishemtov7@gmail.com>
2024-03-30 01:26:50 +0300

    Change axis in cancellations report to white

--- src/server/NotifyThread.java
+++ src/server/NotifyThread.java
@@ -9,20 +9,17 @@ import java.util.Iterator;
 
 import javax.swing.JOptionPane;
 
-
 import DB.DatabaseController;
 import common.Order;
 import common.OrderNotification;
 import common.WaitingList;
 
-
-/** 
+/**
  * NotifyThread class implements Runnable.
  * 
- * This class handle all the automated functionality:
- * Send reminder 24 hours before visit.
- * Cancel the visit if the traveler did not confirmed within two hours.
- * Notify the next in the waiting list
+ * This class handle all the automated functionality: Send reminder 24 hours
+ * before visit. Cancel the visit if the traveler did not confirmed within two
+ * hours. Notify the next in the waiting list
  *
  */
 public class NotifyThread implements Runnable {
@@ -30,25 +27,24 @@ public class NotifyThread implements Runnable {
 	private final int second = 1000;
 	private final int minute = second * 30;
 	private static DatabaseController DC;
-	
-    public static DatabaseController getDC() {
+
+	public static DatabaseController getDC() {
 		return DC;
 	}
 
 	private ArrayList<OrderNotification> ordersWithAlerts; // Array to store orders with alerts
-    private ArrayList<WaitingList> waitingArray;
+	private ArrayList<WaitingList> waitingArray;
 
 	public NotifyThread(DatabaseController DBController) {
 		DC = DBController;
-        ordersWithAlerts = new ArrayList<>(); // Initialize the array
-        waitingArray = new ArrayList<>();
+		ordersWithAlerts = new ArrayList<>(); // Initialize the array
+		waitingArray = new ArrayList<>();
 	}
 
 	/**
-	 * This function handle all the automated functionality:
-	 * Send reminder 24 hours before visit.
-	 * Cancel the visit if the traveler did not confirmed within two hours.
-	 * Notify the next in the waiting list
+	 * This function handle all the automated functionality: Send reminder 24 hours
+	 * before visit. Cancel the visit if the traveler did not confirmed within two
+	 * hours. Notify the next in the waiting list
 	 */
 	@Override
 	public void run() {
@@ -56,34 +52,33 @@ public class NotifyThread implements Runnable {
 		while (true) {
 
 			ArrayList<Order> pendingOrders = DC.getOrdersByStatusInLastTwentyFourHours("PENDING");
-			ArrayList<OrderNotification> ordersAlreadyNotified= DC.getTodayNotificationsWithStatus("SENT");
-			
-			
+			ArrayList<OrderNotification> ordersAlreadyNotified = DC.getTodayNotificationsWithStatus("SENT");
+
 			for (Order order : pendingOrders) {
 				String status = "PENDING_EMAIL_SENT";
 				String orderId = String.valueOf(order.getOrderId());
 				order.setStatus(status);
-				//Change status of orders to PENDING_EMAIL_SENT
+				// Change status of orders to PENDING_EMAIL_SENT
 				DC.updateOrderStatus(order);
-				
-				/**Visitor has 2 hours to accept visit from now*/
+
+				/** Visitor has 2 hours to accept visit from now */
 				OrderNotification createNotification = new OrderNotification(Integer.parseInt(orderId), LocalDate.now(),
 						LocalTime.now(), LocalTime.now().plusHours(2), "SENT");
-				
-				//Add notification to the orders notified arrayList
+
+				// Add notification to the orders notified arrayList
 				ordersAlreadyNotified.add(createNotification);
-				
-				//Posting the notification in the db
+
+				// Posting the notification in the db
 				DC.postOrderNotification(createNotification);
-				
-				WaitingList waiting = new WaitingList(order.getOrderId(), order.getVisitorId(),
-						order.getParkNumber(), order.getAmountOfVisitors(), order.getPrice(),
-						order.getVisitorEmail(), order.getDate(), order.getVisitTime(), order.getOrderStatus(),
-						order.getTypeOfOrder(), order.getTelephoneNumber(), order.getParkName(), 0, 0);
+
+				WaitingList waiting = new WaitingList(order.getOrderId(), order.getVisitorId(), order.getParkNumber(),
+						order.getAmountOfVisitors(), order.getPrice(), order.getVisitorEmail(), order.getDate(),
+						order.getVisitTime(), order.getOrderStatus(), order.getTypeOfOrder(),
+						order.getTelephoneNumber(), order.getParkName(), 0, 0);
 				waitingArray.add(waiting);
-				
+
 			}
-			
+
 			ordersWithAlerts = ordersAlreadyNotified;
 			CancelOrderAndNotify(waitingArray);
 			deleteAlertsExpired();
@@ -96,43 +91,39 @@ public class NotifyThread implements Runnable {
 		}
 	}
 
-	
 	private void CancelOrderAndNotify(ArrayList<WaitingList> waiting) {
-	    
-	    // Iterate through orders with alerts and cancel expired orders
-	    Iterator<OrderNotification> iterator = ordersWithAlerts.iterator();
-	    while (iterator.hasNext()) {
-	    	int i = 0;
-	    	OrderNotification notificationOfSpecificOrder = iterator.next();
-	        if (isAlertExpired(LocalTime.now(), notificationOfSpecificOrder.getEndNotification())) {
-	            // Cancel the order
-	        	System.out.print("Cancel Order");
-	        	
-	        	//Change status in the orderNotification table
-	        	DC.changeStatusOfNotification(notificationOfSpecificOrder.getOrderId(), "PASSED");
-	        	
-	        	//Change status in the order table
-	            DC.updateOrderStatusArray(new ArrayList<String>(Arrays.asList("CANCELEDBYSERVER", String.valueOf(notificationOfSpecificOrder.getOrderId()))));
 
-	            // Remove the canceled order from orderNotifications
-	            WaitingListControl.notifyPersonFromWaitingList(waiting.get(i));
+		// Iterate through orders with alerts and cancel expired orders
+		Iterator<OrderNotification> iterator = ordersWithAlerts.iterator();
+		while (iterator.hasNext()) {
+			int i = 0;
+			OrderNotification notificationOfSpecificOrder = iterator.next();
+			if (isAlertExpired(LocalTime.now(), notificationOfSpecificOrder.getEndNotification())) {
+				// Cancel the order
+				System.out.print("Cancel Order");
 
-	            waiting.remove(0);
-	            iterator.remove();
-	        }
-	    }
+				// Change status in the orderNotification table
+				DC.changeStatusOfNotification(notificationOfSpecificOrder.getOrderId(), "PASSED");
+
+				// Change status in the order table
+				DC.updateOrderStatusArray(new ArrayList<String>(
+						Arrays.asList("CANCELEDBYSERVER", String.valueOf(notificationOfSpecificOrder.getOrderId()))));
+				
+					// Remove the canceled order from orderNotifications
+					WaitingListControl.notifyPersonFromWaitingList(waiting.get(i));
+					waiting.remove(0);
+				
+				iterator.remove();
+			}
+		}
 	}
-	
+
 	private void deleteAlertsExpired() {
-	    DC.deleteExpiredOrderAlerts();
+		DC.deleteExpiredOrderAlerts();
 	}
-	
+
 	private boolean isAlertExpired(LocalTime currentTime, LocalTime alertEndTime) {
-	    return currentTime.isAfter(alertEndTime);
+		return currentTime.isAfter(alertEndTime);
 	}
-
-
-
-
 
 }
